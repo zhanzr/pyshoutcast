3b23bf278466b342b612129ea8f3a51f4b9a76b1
zhanzr <zhanzr@foxmail.com>
2017-08-05 00:59:40 +0800

    New Project.

---
+++ testplay.py
@@ -0,0 +1,14 @@
+#!/bin/python
+
+import os
+import math
+import random
+import audioop
+import winsound
+
+if __name__ == "__main__":
+	print(len(sys.argv))
+	for i in range(len(sys.argv)):
+		print(sys.argv[i])
+		
+
